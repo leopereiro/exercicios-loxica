fb91d11965f5d8d73d7da717c929fc865427cb73
leopereiro <lperpom.genius@gmail.com>
2025-05-20 08:43:45 +0200

    actualización

--- static/javascript/funcions/exercicio8.js
+++ static/javascript/funcions/exercicio8.js
@@ -2,14 +2,14 @@ import {persons, pets} from "../datos/datosEnunciados.js";
 export function persoasConMascotas() {
     let persoasConMascotas = [];
     for (let contador=0; contador < persons.length; contador++) {
+        for (let contador=0; contador < pets.length; contador++) {
             if(persons[contador]) {
                 persoasConMascotas.push(persons[contador].name);
             }
-    }
-    for (let contador=0; contador < pets.length; contador++) {
         if(pets[contador]) {
             persoasConMascotas.push(pets[contador].name && pets[contador].type);
         }
+    }
 }
 return persoasConMascotas;
 }
